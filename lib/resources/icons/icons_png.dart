df29397031f2775dbcfa69100adb5a4c2c928f24
JoseAngelGallardoPerez <79593239+JoseAngelGallardoPerez@users.noreply.github.com>
2021-04-22 17:05:35 -0500

    Add files via upload

---
+++ lib/resources/icons/icons_png.dart
@@ -0,0 +1,7 @@
+class IconsPNG {
+  static const String cardGradientGreen =
+      'assets/images/ic_card_gradient_green.png';
+  static const String cardGradientBlue =
+      'assets/images/ic_card_gradient_blue.png';
+  static const String welcome = 'assets/images/welcome.png';
+}
